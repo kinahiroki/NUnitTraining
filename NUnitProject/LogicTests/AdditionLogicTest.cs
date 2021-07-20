908f49e60e3f177862ec615d24218c70c23b6f43
h.kina <h.kina@dcom-net.jp>
2021-07-20 14:31:55 +0900

    テストプロジェクト新規追加

---
+++ NUnitProject/LogicTests/AdditionLogicTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitProject.LogicTests
+{
+    // ①テストクラスの宣言
+    // (1) [TestFixure]属性を付与⇒このクラスがテストクラスであることを宣言
+    //    ※テストクラスは「public」であること
+
+    [TestFixture]
+    public class AdditionLogicTest
+    {
+        // ②テストケースの追加 Part1
+        // (1) [Test]属性を付与⇒テストを行うメソッドであることを宣言
+        //    ※テストケースは「public」で戻り値なしのメソッドであること
+        // (2) メソッド名は[呼び出すメソッド名] + Test
+
+        [Test]
+        public void AddTest()
+        {
+            // テストケースの記述
+
+        }
+    }
+}
